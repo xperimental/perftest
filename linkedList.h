57e3e04dcfc8faa48228d60521eb021f46596518
Robert <xperimental@solidproject.de>
2009-09-12 17:22:22 +0200

    own implementation of doubly-linked list

---
+++ linkedList.h
@@ -0,0 +1,97 @@
+#ifndef linkedList_H
+#define linkedList_H
+
+template <typename K, typename V> class linkedListNode
+{
+public:
+	linkedListNode(void)
+		: key(), value(), prev(0), next(0)
+	{
+	}
+
+	linkedListNode(const K& newKey)
+		: key(newKey), value(), prev(0), next(0)
+	{
+	}
+
+	K key;
+	V value;
+
+	linkedListNode<K, V>* prev;
+	linkedListNode<K, V>* next;
+};
+
+template <typename K, typename V> class linkedList
+{
+public:
+
+	linkedList(void)
+	{
+		m_head = NULL;
+	}
+
+	~linkedList(void)
+	{
+		clear();
+	}
+
+	V& operator[](const K& key)
+	{
+		linkedListNode<K, V>* node = find(key);
+		if (node == NULL)
+		{
+			node = insert(key);
+		}
+		return node->value;
+	}
+
+	void clear()
+	{
+		while (m_head != NULL)
+		{
+			linkedListNode<K, V>* tmp = m_head;
+			m_head = tmp->next;
+			delete tmp;
+		}
+	}
+
+protected:
+	linkedListNode<K, V>* m_head;
+
+	linkedListNode<K, V>* find(const K& key) const
+	{
+		linkedListNode<K, V>* node = m_head;
+		if (node == NULL)
+			return NULL;
+		while ((key > node->key) && (node->next != NULL))
+		{
+			node = node->next;
+		}
+		if (key == node->key)
+			return node;
+		return NULL;
+	}
+
+	linkedListNode<K, V>* insert(const K& key)
+	{
+		if (m_head == NULL)
+		{
+			m_head = new linkedListNode<K, V>(key);
+			return m_head;
+		}
+		linkedListNode<K, V>* node = m_head;
+		while ((key > node->key) && (node->next != NULL))
+		{
+			node = node->next;
+		}
+		linkedListNode<K, V>* newNode = new linkedListNode<K, V>(key);
+		newNode->prev = node->prev;
+		if (newNode->prev != NULL)
+			newNode->prev->next = newNode;
+		newNode->next = node;
+		node->prev = newNode;
+		return newNode;
+	}
+};
+
+#endif
--- perftest.cpp
+++ perftest.cpp
@@ -6,6 +6,8 @@
 #include <map>
 #include "time.h"
 
+#include "linkedList.h"
+
 using std::cout;
 using std::endl;
 
@@ -19,22 +21,13 @@ int randomIndex()
 	return (int)((double)rand() / (RAND_MAX + 1) * (MAX_ELEMENTS - 1));
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+void stdMap(int* access)
 {
-	cout << "Small performance comparison of different dynamic datastructures." << endl;
-
-	cout << "Creating random access pattern..." << endl;
-	srand( (unsigned)time( NULL ) );
-	int* access = new int[RAND_ACCESS];
-	for (int i = 0; i < RAND_ACCESS; i++)
-	{
-		access[i] = randomIndex();
-	}
+	std::map<int, int> map;
 
 	cout << "std::map:" << endl
 		<< "\tinsertion (" << MAX_ELEMENTS << " elements): ";
 	long time = clock();
-	std::map<int, int> map;
 	for (int i = 0; i < MAX_ELEMENTS; i++)
 	{
 		map[i] = MAX_ELEMENTS - i;
@@ -50,6 +43,66 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
 	time = (clock() - time) / CLOCKS_PER_MS;
 	cout << time << "ms" << endl;
+
+	cout << "\tsequential access: ";
+	time = clock();
+	for (int i = 0; i < MAX_ELEMENTS; i++)
+	{
+		map[i] = MAX_ELEMENTS - i;
+	}
+	time = (clock() - time) / CLOCKS_PER_MS;
+	cout << time << "ms" << endl;
+
+	map.clear();
+}
+
+void list(int* access)
+{
+	linkedList<int, int> list;
+
+	cout << "doubly linked list:" << endl
+		<< "\tinsertion (" << MAX_ELEMENTS << " elements): ";
+	long time = clock();
+	for (int i = 0; i < MAX_ELEMENTS; i++)
+	{
+		list[i] = MAX_ELEMENTS - i;
+	}
+	time = (clock() - time) / CLOCKS_PER_MS;
+	cout << time << "ms" << endl;
+
+	cout << "\trandom access (" << RAND_ACCESS <<" accesses): ";
+	time = clock();
+	for (int i = 0; i < RAND_ACCESS; i++)
+	{
+		list[access[i]] = i;
+	}
+	time = (clock() - time) / CLOCKS_PER_MS;
+	cout << time << "ms" << endl;
+
+	cout << "\tsequential access: ";
+	time = clock();
+	for (int i = 0; i < MAX_ELEMENTS; i++)
+	{
+		list[i] = MAX_ELEMENTS - i;
+	}
+	time = (clock() - time) / CLOCKS_PER_MS;
+	cout << time << "ms" << endl;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Small performance comparison of different dynamic datastructures." << endl;
+
+	cout << "Creating random access pattern..." << endl;
+	srand( (unsigned)time( NULL ) );
+	int* access = new int[RAND_ACCESS];
+	for (int i = 0; i < RAND_ACCESS; i++)
+	{
+		access[i] = randomIndex();
+	}
+
+	stdMap(access);
+	list(access);
 
 	delete[] access;
 	return 0;
