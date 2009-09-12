f91b12e7162fab09856d3082facb70d8e057f30f
Robert <xperimental@solidproject.de>
2009-09-12 16:50:28 +0200

    std::map random access

--- perftest.cpp
+++ perftest.cpp
@@ -9,16 +9,30 @@
 using std::cout;
 using std::endl;
 
-#define MAX_ELEMENTS 100000
+#define MAX_ELEMENTS 1000000
+#define RAND_ACCESS 100000
 
 #define CLOCKS_PER_MS (CLOCKS_PER_SEC / 1000)
+
+int randomIndex()
+{
+	return (int)((double)rand() / (RAND_MAX + 1) * (MAX_ELEMENTS - 1));
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "Small performance comparison of different dynamic datastructures." << endl;
 
+	cout << "Creating random access pattern..." << endl;
+	srand( (unsigned)time( NULL ) );
+	int* access = new int[RAND_ACCESS];
+	for (int i = 0; i < RAND_ACCESS; i++)
+	{
+		access[i] = randomIndex();
+	}
+
 	cout << "std::map:" << endl
-		<< "\tinsertion: ";
+		<< "\tinsertion (" << MAX_ELEMENTS << " elements): ";
 	long time = clock();
 	std::map<int, int> map;
 	for (int i = 0; i < MAX_ELEMENTS; i++)
@@ -28,8 +42,16 @@ int _tmain(int argc, _TCHAR* argv[])
 	time = (clock() - time) / CLOCKS_PER_MS;
 	cout << time << "ms" << endl;
 
-	cout << "Press <RETURN>." << endl;
-	std::cin.get();
+	cout << "\trandom access (" << RAND_ACCESS <<" accesses): ";
+	time = clock();
+	for (int i = 0; i < RAND_ACCESS; i++)
+	{
+		map[access[i]] = i;
+	}
+	time = (clock() - time) / CLOCKS_PER_MS;
+	cout << time << "ms" << endl;
+
+	delete[] access;
 	return 0;
 }
 
