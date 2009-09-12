c04e4ddf735ead6712b7611f325b1ba6e3dc2896
Robert <xperimental@solidproject.de>
2009-09-12 17:59:10 +0200

    Added link to GPL to every source file.

--- perftest.cpp
+++ perftest.cpp
@@ -1,7 +1,22 @@
-// perftest.cpp : Defines the entry point for the console application.
-//
+/*
+This file is part of perftest.
 
-#include "stdafx.h"
+perftest is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+perftest is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with perftest.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <tchar.h>
 #include <iostream>
 #include <map>
 #include "time.h"
